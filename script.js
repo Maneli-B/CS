00c0a6ac1d81641a54f7d0f35181717e7a20dc1a
Maneli <maneli.bahanian@gmail.com>
2025-05-31 14:47:34 -0700

    Added JS

---
+++ script.js
@@ -0,0 +1,9 @@
+let name ="Maneli";
+let age = "13"
+let likesushi = "false"
+function greeter(name) {
+    console.log("Hello", name);
+
+}
+greeter('username');
+let newName = document.getElementbyId('greeter').value;
