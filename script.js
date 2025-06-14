730d73b0ec4d6b8c6f0a47cc95de96867afd788a
Maneli <maneli.bahanian@gmail.com>
2025-06-14 14:58:12 -0700

    3.0

--- script.js
+++ script.js
@@ -12,9 +12,10 @@ function savename() {
     document.getElementById('username').textContent = newName;
     document.getElementById("greeter").value = "";
 }
+
 document.querySelectorAll('.accordion-header').forEach(function(button){
     button.addEventListener('click',function() {
-let content = this.nextElementSibling;
-(content.style.display === 'block') ?'none' : 'block' ;
+        let content = this.nextElementSibling;
+        content.style.display = (content.style.display === 'block') ?'none' : 'block' ;
     });
 });
