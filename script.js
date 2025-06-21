4084d1f84118ee44b26d6619de9fe1689d290214
Maneli <maneli.bahanian@gmail.com>
2025-06-21 11:51:32 -0700

    3.1

--- script.js
+++ script.js
@@ -19,3 +19,20 @@ document.querySelectorAll('.accordion-header').forEach(function(button){
         content.style.display = (content.style.display === 'block') ?'none' : 'block' ;
     });
 });
+
+const PASSWORD = "Maneli1390";
+
+ function ValidatePass() {
+    const login = document.getElementById('password');
+    const message = document.getElementById('message');
+    const hiddenEntries = document.querySelectorAll(".hidden");
+
+    if ( login.value === PASSWORD) {
+        message.textContent='Password confirmed see member only content below.';
+        hiddenEntries.forEach(function(entry) {entry.style.display = 'flex';});
+    }else{    message.textContent= 'Password denied. Try again.';
+        hiddenEntries.forEach(function(entry) {entry.style.display = 'none ';});    
+    }
+
+    login.value ='';
+ }
